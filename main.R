cf2fd6a5e8699f118923fee9c6137685e127f18d
Adithi0507 <adithi.hamsi@gmail.com>
2024-09-17 20:02:29 -0400

    Update main.R

--- main.R
+++ main.R
@@ -18,8 +18,7 @@
 #' less_than_zero(c(-1,0,1,2,3,4))
 #' [1] TRUE FALSE FALSE FALSE FALSE FALSE
 less_than_zero <- function(x) {
-    x <- 2
-    return(x)
+   return(x < 2)
 }
 
 #' Evaluate whether the argument is between two numbers
@@ -46,7 +45,7 @@ less_than_zero <- function(x) {
 #' [3,] FALSE FALSE FALSE
 is_between <- function(55, 50, 60) {
     
-    return(TRUE)
+    return(x > a & x < b)
 }
 
 #' Return the values of the input vector that are not NA
@@ -63,7 +62,7 @@ is_between <- function(55, 50, 60) {
 #' rm_na(x)
 #' [1] 1 2 3
 rm_na <- function(x) {
-    return(NULL)
+    return(x[!is.na(x)])
 }
 
 #' Calculate the median of each row of a matrix
@@ -82,7 +81,7 @@ rm_na <- function(x) {
 #' [1] 1 4 7
 #' 
 row_medians <- function(x) {
-    return(NULL)
+    return(apply(x, 1, median, na.rm = TRUE))
 }
 
 #' Evaluate each row of a matrix with a provided function
@@ -107,7 +106,7 @@ row_medians <- function(x) {
 #' summarize_rows(m, mean)
 #' [1] 2 5 8
 summarize_rows <- function(x, fn, na.rm=FALSE) {
-    return(NULL)
+    return(apply(x, 1, fn, na.rm = na.rm))
 }
 
 #' Summarize matrix rows into data frame
@@ -147,7 +146,20 @@ summarize_rows <- function(x, fn, na.rm=FALSE) {
 #' 3 -0.09040182 1.027559 -0.02774705 -3.026888 2.353087      130              54      0
 #' 4  0.09518138 1.030461  0.11294781 -3.409049 2.544992       90              72      0
 summarize_matrix <- function(x, na.rm=FALSE) {
-    return(NULL)
+    num_lt_0 <- apply(x, 1, function(row) sum(row < 0, na.rm = na.rm))
+    num_btw_1_and_5 <- apply(x, 1, function(row) sum(row > 1 & row < 5, na.rm = na.rm))
+    num_na <- apply(x, 1, function(row) sum(is.na(row)))
+    
+    data.frame(
+        mean = rowMeans(x, na.rm = na.rm),
+        stdev = apply(x, 1, sd, na.rm = na.rm),
+        median = apply(x, 1, median, na.rm = na.rm),
+        min = apply(x, 1, min, na.rm = na.rm),
+        max = apply(x, 1, max, na.rm = na.rm),
+        num_lt_0 = num_lt_0,
+        num_btw_1_and_5 = num_btw_1_and_5,
+        num_na = num_na
+    )
 }
 
 # ------------ Helper Functions Used By Assignment, You May Ignore ------------
